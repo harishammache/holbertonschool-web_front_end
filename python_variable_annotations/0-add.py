2c904c982f85d17ee5b00fd6586d2639fa5b9190
harishammache <hammache_haris@hotmail.fr>
2024-04-22 09:12:39 +0000

    0. Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    function add that takes a float a and a float b
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        Add two floating point numbers.
+
+        Parameters:
+            a (float): The first floating point number.
+            b (float): The second floating point number.
+
+        Returns:
+            float: The sum of the two floating point numbers.
+    """
+    return a + b
