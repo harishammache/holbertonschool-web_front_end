dd7e968f4705ae58c70302b254074ceedb274df9
harishammache <hammache_haris@hotmail.fr>
2025-05-19 13:23:00 +0200

    chore: setup project with Webpack configuration and dependencies

---
+++ Cookies_local_storage/webpack.config.js
@@ -0,0 +1,19 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
+    },
+    port: 8081,
+    open: true,
+  },
+  mode: 'development',
+};
